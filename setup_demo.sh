b0ac351d9a48f91870c67cf408230d74d9d89bc8
Alan Chung Ma <achungma@purdue.edu>
2023-04-28 13:59:43 -0400

    create setup demo file

---
+++ setup_demo.sh
@@ -0,0 +1,12 @@
+# Install demo dependencies
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Install monkey-patched in-toto
+git submodule update --init
+cd in-toto-layout-generator
+pip install .
+cd ..
+
+echo "\nMake sure to run 'source venv/bin/activate' before starting the demo!"
